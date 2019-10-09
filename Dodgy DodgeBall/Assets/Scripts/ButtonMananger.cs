5f8c2bf5278331b6abc1225bfe744dad7aa77e41
ConnorY97 <47731551+ConnorY97@users.noreply.github.com>
2019-10-09 13:40:48 +1100

    Attempt 2 at committing

---
+++ Dodgy DodgeBall/Assets/Scripts/ButtonMananger.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonMananger : MonoBehaviour
+{
+    //public Button PlayButton;
+    //private GameObject OptionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MainMenu = GameObject.FindGameObjectWithTag("MainMenu");
+        //OptionsMenu = GameObject.FindGameObjectWithTag("OptionsMenu");
+        //OptionsMenu.SetActive(false);
+        //MainMenu.SetActive(true);
+
+        //PlayButton = GetComponent<Button>(); 
+
+    }
+
+    [SerializeField] GameObject[] buttons;
+
+    // Try attacking a script with this function in it to the seperate buttons and see if that does anything
+    // link: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnMouseEnter.html
+    private void OnMouseOver()
+    {
+        // Change colour
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject button in buttons)
+        {
+            Button currentButton = button.GetComponent<Button>();
+            ColorBlock cb = currentButton.colors;
+            cb.highlightedColor = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+            currentButton.colors = cb;
+        }
+        Debug.Log("FUCK");
+        //Debug.Log(active.name); 
+
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("SHIT");
+        
+    }
+
+
+    private void OnMouseExit()
+    {
+        GameObject active = EventSystem.current.currentSelectedGameObject;
+        if (active != null)
+        {
+            //Debug.Log("FUCK");
+            Button currentButton = active.GetComponent<Button>();
+            ColorBlock cb = currentButton.colors;
+            cb.normalColor = Color.black;
+        }
+    }
+
+
+
+
+    //public void Play()
+    //{
+    //    MainMenu.SetActive(false);  
+    //}
+
+    //public void Options()
+    //{
+    //    MainMenu.SetActive(false);
+    //    OptionsMenu.SetActive(true); 
+    //}
+
+    //public void OptionsBack()
+    //{
+    //    MainMenu.SetActive(true);
+    //    OptionsMenu.SetActive(false); 
+    //}
+}
+
+public interface IPointerEnterHandler : IEventSystemHandler
+{
+    
+}
---
+++ Dodgy DodgeBall/Assets/Scripts/Heading.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heading : MonoBehaviour
+{
+    public Image HeadingImage;
+
+    float TimeLeft;
+    Color TargetColour; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetColour = new Color(1.0f, 0.0f, 0.0f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HeadingImage.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
+
+        if (TimeLeft <= Time.deltaTime)
+        {
+            HeadingImage.color = TargetColour;
+
+            TargetColour = new Color(Random.value, Random.value, Random.value);
+            TimeLeft = 1;
+        }
+        else
+        {
+            HeadingImage.color = Color.Lerp(HeadingImage.color, TargetColour, Time.deltaTime / TimeLeft);
+
+            TimeLeft -= Time.deltaTime;
+        }
+    }
+}
