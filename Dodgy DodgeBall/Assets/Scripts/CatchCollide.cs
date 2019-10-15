43aca079721c6ae6407f5096ab2ca2826db112e0
LeithMerrifield <47731609+LeithMerrifield@users.noreply.github.com>
2019-10-15 14:21:11 +1100

    Added RoundManager and Team manager -- Global isplaying bool

---
+++ Dodgy DodgeBall/Assets/Scripts/CatchCollide.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+/// Description: Handles ball entering player catch range
+/// Authors Leith Merrifield
+/// Creation Date: 07/10/2019
+/// Modified: 07/10/2019
+///</summary>
+
+
+public class CatchCollide : MonoBehaviour
+{
+    public GameObject m_player = null;
+    private GameObject m_lastBall = null;
+    private bool m_cooldown = false;
+
+    private int m_pNumber = 1;
+
+   //public int m_PlayerControllerNumber;
+
+    void Start()
+    {
+        if (m_player == null)
+        {
+            Debug.LogAssertion("You need to set a player for the Catch Collide script \n" +
+                                "Name: " + gameObject.name +
+                                ", Parent: " + transform.parent.name);
+        }
+    }
+
+    public void Update()
+    {
+        m_pNumber = gameObject.transform.parent.GetComponent<Player>().m_PlayerControllerNumber;
+    }
+
+    // When an object is colliding with the catch range
+    void OnTriggerStay(Collider other)
+    {
+        if (m_cooldown)
+            return;
+        
+        if(other.gameObject.layer == 9 && Input.GetKey(KeyCode.Mouse0) || Input.GetAxis("P" + m_pNumber + " Right Trigger") > 0 && other.tag == "Ball")
+        {
+            m_player.GetComponent<Player>().SetBall(other.gameObject);
+            //Debug.Log("SetBall");
+
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetAxis("P" + m_pNumber + " Right Trigger") == 0)
+            { 
+                m_lastBall = other.gameObject;
+                StartCoroutine(cooldown());
+            }
+        }
+    }
+
+    IEnumerator cooldown()
+    {
+        m_cooldown = true;
+        yield return new WaitForSeconds(.5f);
+        m_cooldown = false;
+    }
+    
+}
---
+++ Dodgy DodgeBall/Assets/Scripts/TeamManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour
+{
+    public static List<GameObject> m_blueTeam = new List<GameObject>();
+    public static List<GameObject> m_redTeam = new List<GameObject>();
+    public GameObject m_playerPrefab = null;
+    public List<GameObject> m_blueSpawns = new List<GameObject>();
+    public List<GameObject> m_redSpawns = new List<GameObject>();
+
+    public Material m_redMaterial = null;
+    public Material m_blueMaterial = null;
+
+    ///<summary>
+    /// team: 1 = blue team,  2 = red team
+    ///</summary>
+    void AddPlayer(int team, GameObject player)
+    {
+        switch(team)
+        {
+            case 1:
+                m_blueTeam.Add(player);
+                player.GetComponent<MeshRenderer>().material = m_blueMaterial;
+                break;
+            case 2:
+                m_redTeam.Add(player);
+                player.GetComponent<MeshRenderer>().material = m_redMaterial;
+                break;
+            default:
+                Debug.LogError("Adding player to a nonexistent team");
+                break;
+        }
+    }
+
+    ///<summary> Resets spawns/isAlive </summary>
+    public void Reset()
+    {
+        int idx = 0;
+        foreach(var player in m_blueTeam)
+        {
+            player.transform.position = m_blueSpawns[idx].transform.position;  
+            player.GetComponent<Player>().m_isAlive = true;
+            player.SetActive(true);
+            idx++;
+        }
+        idx = 0;
+        foreach(var player in m_redTeam)
+        {
+            player.transform.position = m_redSpawns[idx].transform.position;
+            player.GetComponent<Player>().m_isAlive = true;
+            player.SetActive(true);
+            idx++;
+        }
+    }
+
+    void Start()
+    {
+        // This only here in place of a team select
+        m_blueTeam.Add(Instantiate(m_playerPrefab,m_blueSpawns[0].transform.position, Quaternion.identity));
+        m_blueTeam.Add(Instantiate(m_playerPrefab,m_blueSpawns[1].transform.position, Quaternion.identity));
+        m_redTeam.Add(Instantiate(m_playerPrefab,m_redSpawns[0].transform.position, Quaternion.identity));
+        m_redTeam.Add(Instantiate(m_playerPrefab,m_redSpawns[1].transform.position, Quaternion.identity));
+
+        foreach(var i in m_blueTeam)
+            i.GetComponent<MeshRenderer>().material = m_blueMaterial;
+        foreach(var i in m_redTeam)
+            i.GetComponent<MeshRenderer>().material = m_redMaterial;
+    }
+}
