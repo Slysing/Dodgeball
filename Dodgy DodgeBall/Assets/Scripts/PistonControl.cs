1cdbc54dc7ac84af9456d54985fedf0e022ec04b
LeithMerrifield <47731609+LeithMerrifield@users.noreply.github.com>
2019-11-11 13:21:49 +1100

    Pistons

--- Dodgy DodgeBall/Assets/Scripts/PistonControl.cs
+++ Dodgy DodgeBall/Assets/Scripts/PistonControl.cs
@@ -6,19 +6,25 @@
  */
 
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
 
 public class PistonControl : MonoBehaviour
 {
-    public Animator anim = null;
+    public Animator m_anim = null;
+    public List<Animator> m_animLights = new List<Animator>();
     public float m_interval = 15.0f;
     public float m_holdInterval = 10.0f; // How long the pistons will stay up
     public bool m_togglePistons = true;
 
+    private bool m_extend = false;
     private float m_count = 0.0f;
     private bool m_playingBool = false; // if the animation is playing/up
 
     private void Update()
     {
+
         if (RoundManager.m_isPlaying && m_togglePistons)
         {
             //print("count: " + m_count);
@@ -27,7 +33,13 @@ public class PistonControl : MonoBehaviour
                 m_count += Time.deltaTime;
                 if ((int)m_count % (int)m_interval == 0 && (int)m_count != 0)
                 {
-                    anim.SetTrigger("Extend");
+                    StartCoroutine(PistonRaise());
+
+                    foreach(Animator piston in m_animLights)
+                    {
+                        piston.SetTrigger("Extend");     
+                    }     
+                    m_extend = true;
                     m_playingBool = true;
                     m_count = 0.0f;
                 }
@@ -37,7 +49,13 @@ public class PistonControl : MonoBehaviour
                 m_count += Time.deltaTime;
                 if ((int)m_count % (int)m_holdInterval == 0 && (int)m_count != 0)
                 {
-                    anim.SetTrigger("Retract");
+                    StartCoroutine(PistonLower());
+                    m_anim.SetTrigger("Retract");     
+                    foreach(Animator piston in m_animLights)
+                    {
+                        piston.SetTrigger("Extend");     
+                    }                   
+                    m_extend = false;
                     m_playingBool = false;
                     m_count = 0.0f;
                 }
@@ -52,10 +70,26 @@ public class PistonControl : MonoBehaviour
             !RoundManager.m_redTeamAlive) &&
             m_playingBool == true)
         {
-            anim.SetTrigger("Down");
+            StartCoroutine(PistonLower());
+            foreach(Animator piston in m_animLights)
+            {
+                piston.SetTrigger("Extend");     
+            }       
+
             m_count = 0;
             m_playingBool = false;
             print("Yee");
         }
+    }
+
+    private IEnumerator PistonRaise()
+    {
+        yield return new WaitForSeconds(1.0f);
+        m_anim.SetTrigger("Extend");
+    }
+    private IEnumerator PistonLower()
+    {
+        yield return new WaitForSeconds(1.0f);
+        m_anim.SetTrigger("Retract");
     }
 }
