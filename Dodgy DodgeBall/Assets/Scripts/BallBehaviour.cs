94204e9810cb81d1cd176e0e3b0ad23894dca3ef
ConnorY97 <47731551+ConnorY97@users.noreply.github.com>
2019-11-19 11:43:33 +1100

    Added BallBehaviour script

---
+++ Dodgy DodgeBall/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    public GameObject m_owner = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
